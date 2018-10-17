14f630b5560e013c160de6a6d6de7f2f59465704
pmurp <pmurp@YogaFire-LP>
2018-10-17 18:02:50 -0500

    Where

--- src/test/runner/MainRunner.java
+++ src/test/runner/MainRunner.java
@@ -13,8 +13,6 @@ import cucumber.api.junit.Cucumber;
 		tags = {},
 		plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
 		)
-
-
 public class MainRunner {
 		
 
